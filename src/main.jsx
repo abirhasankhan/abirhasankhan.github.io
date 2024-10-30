ab5c7b75549a5127f812ef98e0ae50ca31ddca87
Abir Khan <ak99.abirkhan@gmail.com>
2024-10-31 01:23:54 +0600

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,40 +3,36 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import {
-	RouterProvider,
-	createBrowserRouter,
-	createRoutesFromElements,
+	HashRouter as Router, // Use HashRouter for GitHub Pages
 	Route,
+	Routes,
 } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import ProjectDetail from "./pages/ProjectDetail";
 import Contact from "./pages/Contact";
-import { ThemeProvider } from "./context/ThemeContext"; // Import ThemeProvider
+import { ThemeProvider } from "./context/ThemeContext";
 import Skill from "./pages/Skill";
 
-// Define routes
-const router = createBrowserRouter(
-	createRoutesFromElements(
-		<Route path="/" element={<App />}>
-			<Route index element={<Home />} />
-			<Route path="about" element={<About />} />
-			<Route path="projects" element={<Projects />} />
-			<Route path="skill" element={<Skill />} />
-			<Route path="projects/:id" element={<ProjectDetail />} />
-			<Route path="contact" element={<Contact />} />
-		</Route>
-	)
-);
-
-// Render the app
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<ThemeProvider>
-			{" "}
-			{/* Wrap the app in ThemeProvider */}
-			<RouterProvider router={router} />
+			<Router>
+				<Routes>
+					<Route path="/" element={<App />}>
+						<Route index element={<Home />} />
+						<Route path="about" element={<About />} />
+						<Route path="projects" element={<Projects />} />
+						<Route path="skill" element={<Skill />} />
+						<Route
+							path="projects/:id"
+							element={<ProjectDetail />}
+						/>
+						<Route path="contact" element={<Contact />} />
+					</Route>
+				</Routes>
+			</Router>
 		</ThemeProvider>
 	</StrictMode>
 );
