6e7c3d17cc6a4833d40f797bb0e155f5eddc5a37
Abir Khan <ak99.abirkhan@gmail.com>
2024-10-31 01:41:31 +0600

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/',  // Set this to your GitHub repo name
+  base: '/portfolio/', // Set to your repo name to generate correct paths
   build: {
     outDir: 'dist', // Ensures output is in the correct folder
   },
