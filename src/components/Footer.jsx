b9701cda49430809175b0924f2619e6de4b4fe8d
Abir Khan <ak99.abirkhan@gmail.com>
2025-05-20 15:25:28 +0600

    footer year updaed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -85,12 +85,13 @@ const Footer = () => {
 				<hr className="my-6 border-gray-200 dark:border-gray-700 sm:mx-auto lg:my-8" />
 				<div className="sm:flex sm:items-center sm:justify-between">
 					<span className="text-sm text-gray-500 dark:text-gray-400 sm:text-center">
-						© 2024
+						© {new Date().getFullYear()}{" "}
 						<a href="" className="hover:underline">
 							@abir khan | by AceAres
 						</a>
 						. All Rights Reserved.
 					</span>
+
 					<div className="flex mt-4 space-x-5 sm:justify-center sm:mt-0">
 						<Link
 							to="#"
