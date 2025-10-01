f004e7188d7e5fc5123275bdcd353acf8e4b931a
Abir Khan <ak99.abirkhan@gmail.com>
2025-10-01 17:29:14 +0600

    update the vite config

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // Include .PNG files
+  // Include image files
   assetsInclude: ['**/*.png', '**/*.PNG', '**/*.jpg', '**/*.JPG'],
-  base: '/portfolio/', // Ensure this matches your GitHub repository name
+  base: '/', // Serve from root, not /portfolio/
   build: {
-    outDir: 'dist', // Ensures output is in the correct folder
+    outDir: 'dist', // Output folder
   },
 });
