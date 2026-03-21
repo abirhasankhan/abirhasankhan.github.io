414ecec93963b5f775f4c76e4f3642ea1d369d53
Abir Khan <ak99.abirkhan@gmail.com>
2026-03-22 02:31:06 +0600

    add page

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import "./index.css";
 import App from "./App";
 import Portfolio from "./pages/Portfolio";
 import Jobakun from "./pages/Jobakun";
+import SprozPlayerPage from "./pages/wp-plugin/SprozPlayerPage";
+
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
@@ -17,8 +19,14 @@ createRoot(document.getElementById("root")).render(
 
 					{/* Full separate page */}
 					<Route path="jobakun" element={<Jobakun />} />
+
+					{/* {Plugin route} */}
+					<Route
+						path="/sproz-music-player"
+						element={<SprozPlayerPage />}
+					/>
 				</Route>
 			</Routes>
 		</Router>
-	</StrictMode>
+	</StrictMode>,
 );
